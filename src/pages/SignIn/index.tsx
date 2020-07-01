b7080b59f3dc01b83573a8dc11a7681b12618dac
Cristiano <cristianomorgante@hotmail.com>
2020-07-01 20:35:09 -0300

    Inicial configs and Add navigation configurations

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import * as S from "./styled";
+
+const SignIn: React.FC = () => {
+  return (
+    <S.Container>
+      <View>
+        <Text>SignIn</Text>
+      </View>
+    </S.Container>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import * as S from "./styled";
+
+const SignUp = () => {
+  return (
+    <S.Container>
+      <View>
+        <Text>SignUp</Text>
+      </View>
+    </S.Container>
+  );
+};
+
+export default SignUp;
