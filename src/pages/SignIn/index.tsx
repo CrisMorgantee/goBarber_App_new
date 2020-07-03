6a82655d47dc2ecb97033f0204a4d7d604e0d941
Cristiano <cristianomorgante@hotmail.com>
2020-07-03 00:15:36 -0300

    Up

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image } from "react-native";
+import logo from "../../assets/logo.png";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 import * as S from "./styled";
 
 const SignIn: React.FC = () => {
   return (
     <S.Container>
-      <View>
-        <Text>SignIn</Text>
-      </View>
+      <Image source={logo} />
+      <S.Title>Faça seu logon</S.Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
     </S.Container>
   );
 };
